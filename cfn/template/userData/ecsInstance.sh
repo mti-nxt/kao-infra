ea8b15c3f57c80ca7f8b37909dfe7a55d74f5b55
nishikawa_sh <nishikawa_sh@mti.co.jp>
2016-06-02 18:21:43 +0900

    ECSではなく、Amazon Linux上で直接Dockerイメージを実行するようにしてみた

--- cfn/template/userData/ecsInstance.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-yum update -y
-echo ECS_CLUSTER={"Ref":"KaoCluster"}>> /etc/ecs/ecs.config
-echo ECS_AVAILABLE_LOGGING_DRIVERS=[\"json-file\",\"syslog\",\"awslogs\"]>> /etc/ecs/ecs.config
